5fcedbe310e3c3aa80e1bd5cc100afc49f6d2000
piano-man <icm2015003@iiita.ac.in>
2019-03-02 14:33:48 +0530

    initial config

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProfRate
+An easy to use anonymous rating system allowing students to rate the professors and courses in their college anonymously(Developed for IIIT Allahabad).
---
+++ backend/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cors = require('cors')
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs')
+var uniqid = require('uniqid');
+var http = require('http').createServer(app);
+app.use(cors())
+app.use(bodyParser.json());
+app.use(express.static('./Public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+http.listen(process.env.PORT || 5000);
+var mongo = require('mongodb').MongoClient;
+require('dotenv').config();
+
